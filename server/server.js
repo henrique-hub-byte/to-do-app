a787887a81704fa1c6ee42bf86db7181e7e61668
Luis Henrique Martins dos Santos Bonfim <luis.bonfim@iesde.com.br>
2023-05-09 17:49:29 -0300

    second commit

--- server/server.js
+++ server/server.js
@@ -6,25 +6,23 @@ import cors from 'cors';
 const PORT = process.env.PORT ?? 8000;
 const app = express();
 app.use(cors())
+
 app.get('/todos/:userEmail', async (req, res) => {
    
     const { userEmail } = req.params;
-    console.log('*********')
-    console.log(userEmail)
-    console.log('*********')
-  
+    
     try {
     //const todos = await pool.query('SELECT * FROM todos where user_email = $1', [userEmail])
-    const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+    const todos = await pool.query('SELECT * FROM todos WHERE user_email = ?', [userEmail])
 
     res.setHeader('Content-Type', 'application/json');
-    console.log(todos)
+
     //console.log('----------');
     //console.log(todos[0]);
     res.json(todos.row)
   } catch(err) {
     console.log(err)
-  }
+  } 
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
